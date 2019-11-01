1959347a9400df84b7f1fd7f76cf2dc497262b8a
DaniPani <panero.daniel@gmail.com>
2019-11-01 18:22:42 +0100

    added option to save to .dat files

--- plot_1d.py
+++ plot_1d.py
@@ -8,10 +8,14 @@ stokes = np.array(fits.open('data/tmp.1d.fits')[0].data)
 
 fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(10,10))
 ax = ax.flatten()
+
+label = ['i', 'q', 'u', 'v']
+
 for i in range(4):
 
     ax[i].plot(data['spec1']['wavelength'][:], stokes[i,:])
     for j in range(2):
+        np.savetxt('txt/{}_{}.dat'.format(label[i], j + 1), list(zip(data['spec1']['wavelength'][:], data['spec1']['stokes'][0,0,j,i,:])) )
         ax[i].plot(data['spec1']['wavelength'][:], data['spec1']['stokes'][0,0,j,i,:],  label='Cycle {0}'.format(j))
 
 for i in range(4):
